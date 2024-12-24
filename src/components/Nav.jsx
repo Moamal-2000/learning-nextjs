765fc4efeec5dd7f96d5090605e5cea2a10bcc7a
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-24 07:22:46 +0300

    feat: enhance navigation structure and add Forgot Password page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,23 @@ import Link from "next/link";
 const nav = () => {
   return (
     <nav className="nav">
-      <Link href="/">Home</Link>
-      <Link href="/posts">Posts</Link>
-      <Link href="/about">About</Link>
-      <Link href="/docs">Docs</Link>
-      <Link href="/about/moamal">Me</Link>
+      <div className="routes">
+        <Link href="/">Home</Link>
+        <Link href="/posts">Posts</Link>
+        <Link href="/about">About</Link>
+        <Link href="/docs">Docs</Link>
+        <Link href="/about/moamal">Me</Link>
+      </div>
+
+      <div className="auth">
+        <span>(</span>
+        <div className="wrapper">
+          <Link href="/login">Login</Link>
+          <Link href="/signup">Sign up</Link>
+          <Link href="/forgot-password">Forgot password?</Link>
+        </div>
+        <span>)</span>
+      </div>
     </nav>
   );
 };
