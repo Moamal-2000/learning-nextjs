433348c134b9eee0f47e63e2254f295fd97bd168
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-27 12:16:23 +0300

    feat: add warning message for posts page

--- src/app/(pages)/posts/page.js
+++ src/app/(pages)/posts/page.js
@@ -16,6 +16,12 @@ const Posts = async () => {
           <li key={id}>{title}</li>
         ))}
       </ul>
+
+      {!posts && (
+        <p className="warning">
+          No posts found or there is an error while getting posts
+        </p>
+      )}
     </div>
   );
 };
