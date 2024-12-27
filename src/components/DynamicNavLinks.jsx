02120f5ad10a96724e381b37bea30cb63c85abb6
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-27 11:51:38 +0300

    learning: how to style active link

--- src/components/DynamicNavLinks.jsx
+++ src/components/DynamicNavLinks.jsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const DynamicNavLinks = ({ linksData }) => {
+  const activePath = usePathname();
+
   return linksData.map(({ name, href, children }) => {
+    const activeClass = activePath === href ? "active" : "";
+
     return (
       <>
-        <Link key={name} href={href}>
+        <Link key={name} href={href} className={activeClass}>
           {name}
         </Link>
         {children && <DynamicNavLinks linksData={children} />}
