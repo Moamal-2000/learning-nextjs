8df18106c23695cacb370991df72e50db305af45
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-30 12:59:36 +0300

    learning: parallel routes

--- src/app/(pages)/dashboard/layout.js
+++ src/app/(pages)/dashboard/layout.js
@@ -1,5 +1,11 @@
-const DashboardLayout = ({ children }) => {
-  return <div>{children}</div>;
+const DashboardLayout = ({ children, notifications, user_profile }) => {
+  return (
+    <div>
+      <div className="parallel-card">{children}</div>
+      <h2 className="parallel-card">{notifications}</h2>
+      <h2 className="parallel-card">{user_profile}</h2>
+    </div>
+  );
 };
 
 export default DashboardLayout;
