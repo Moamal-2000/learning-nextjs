44e8f52c72a7982335fa06392b0b97c12e69de80
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-11 17:12:43 +0300

    save comments to the local storage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddMessageForm from "./AddMessageForm";
 import Messages from "./Messages";
 
 const Form = ({ testData }) => {
-  const [dataState, setDataState] = useState(testData);
+  const dataFromLocal = localStorage.getItem("comments");
+  const initData = dataFromLocal ? JSON.parse(dataFromLocal) : testData;
+  const [dataState, setDataState] = useState(initData);
+
+  useEffect(() => {
+    localStorage.setItem("comments", JSON.stringify(dataState));
+  }, [dataState]);
 
   return (
     <>
