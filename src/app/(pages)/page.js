f9cc6cf5bc679faff47714b12f1e5abf72089d7b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-05 07:36:07 +0300

    feat: add ClientComponent to Home page

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,5 +1,11 @@
+import ClientComponent from "@/components/ClientComponent";
 import s from "../page.module.scss";
 
 export default function Home() {
-  return <main className={s.home}></main>;
+  return (
+    <main className={s.home}>
+      <h2>Home page</h2>
+      <ClientComponent />
+    </main>
+  );
 }
